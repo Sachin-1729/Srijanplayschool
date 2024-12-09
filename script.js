00db0dfb2f1a6330f0ed4e43bd4abc54ef1da982
Sachin Tiwari <141061271+Sachin-1729@users.noreply.github.com>
2024-12-09 19:13:10 +0530

    Update script.js
    Song played feature

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const audio = document.querySelector('#audio');
+  let hasInteracted = false;
+
+  // First user interaction (click anywhere on the page)
+  window.addEventListener('click', () => {
+    if (!hasInteracted) {
+      hasInteracted = true;
+      console.log('User interaction detected, autoplay is enabled!');
+    }
+  });
+
+  // Scroll event listener
+  window.addEventListener('scroll', () => {
+    if (hasInteracted && audio.paused) {
+      audio.play().then(() => {
+        console.log('Audio is playing.');
+      }).catch(error => {
+        console.log('Autoplay failed due to browser restrictions:', error);
+      });
+    }
+  });
+
 let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
